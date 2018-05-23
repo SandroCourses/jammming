9cf5a2070970a047504fbe82267cd129329a7c25
SandroCourses <sandroneumann@me.com>
2018-05-23 22:41:49 +0200

    Set up index.html, create header of app and top level css

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Hello, world!</h1>
+        <header>Ja<span class="highlight">mmm</span>ing</header>
       </div>
     );
   }
